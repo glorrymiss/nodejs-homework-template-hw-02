bd81a09ed8667ae7137a7c062ebda50a9aee4ea4
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-06-20 11:52:07 +0200

    add corrects

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,11 +1,11 @@
-const { isValidOdjectId } = require("mongoose");
+const { isValidObjectId } = require("mongoose");
 
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
   const { id } = req.params;
-  if (!isValidOdjectId(id)) {
-    next(HttpError(400, `${id} is not id`));
+  if (!isValidObjectId(id)) {
+    next(HttpError(400, `${id} is not valid id`));
   }
   next();
 };
